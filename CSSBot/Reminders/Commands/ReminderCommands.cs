389627ec9c091431e30c816376a6b1669305085c
Chris Johnston <githubchrisjohnston@gmail.com>
2017-09-24 18:54:45 -0700

    Add files

---
+++ CSSBot/Reminders/Commands/ReminderCommands.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSSBot.Reminders.Commands
+{
+    [Group("Reminder"), Alias("R")]
+    public class ReminderCommands : ModuleBase
+    {
+    }
+}
--- CSSBot/Reminders/ReminderService.cs
+++ CSSBot/Reminders/ReminderService.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace CSSBot.Reminders
 {
-    class ReminderService
+    public class ReminderService
     {
+
     }
 }
