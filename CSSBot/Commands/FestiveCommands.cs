c91208f8340487cd93f458a68335c637393b367a
Chris Johnston <githubchrisjohnston@gmail.com>
2017-11-02 01:21:00 -0700

    added thanksgiving themed emoji

--- CSSBot/Commands/FestiveCommands.cs
+++ CSSBot/Commands/FestiveCommands.cs
@@ -15,7 +15,8 @@ namespace CSSBot.Commands
         {
             // why is there an emoji for snowman with snow
             // and snowman without snow
-            "🌟", "❄️", "☃️", "⛄", "⛄", "🎄", "🕎", "✡️", "🌙"
+            "🌟", "❄️", "☃️", "⛄", "⛄", "🎄", "🕎", "✡️", "🌙",
+            "🦃", "🍁"
         };
 
         private bool CheckDate()
