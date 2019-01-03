7ff2ae6c04df0a420398f6f5dfa9333e7823ade4
Chris Johnston <chjohnston@protonmail.com>
2019-01-03 08:31:40 -0800

    Add support for editing commands after they are first sent

---
+++ CSSBot/Commands/RetryExampleCommand.cs
@@ -0,0 +1,21 @@
+﻿using CSSBot.Services;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSBot.Commands
+{
+    public class RetryExampleCommand : RetryModuleBase
+    {
+        public RetryExampleCommand(MessageRetryService retry) : base(retry)
+        { }
+
+        [Command("Test")]
+        public async Task TestRetry(int a, int b)
+        {
+            await ReplyOrUpdateAsync($"{a + b}");
+        }
+    }
+}
